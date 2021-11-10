7b6a001180832b43bc9f371363459b696a629951
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-11-10 14:46:50 -0300

    enviando projeto final

--- src/app.ts
+++ src/app.ts
@@ -2,40 +2,40 @@ import "dotenv/config";
 import express from "express";
 import http from "http";
 import cors from "cors";
-import { Server, Socket } from "socket.io";
+
+import { Server } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
-app.use(cors())
-const serverHTTP = http.createServer(app);
+app.use(cors());
 
-const io = new Server(serverHTTP, {
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
     cors: {
-        origin: "*"
-    }
-})
+        origin: "*",
+    },
+});
 
-io.on("connection", socket => {
-    console.log(`Usuario conectado no socket ${socket.id}`)
-})
-
-const PORT = process.env.PORT || 4000;
-const clinet_id_oauth = process.env.OAUTH_GIT_HUB_CLIENT_ID;
+io.on("connection", (socket) => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 app.use(express.json());
 
 app.use(router);
 
-//criando rota de auteticação
 app.get("/github", (request, response) => {
-    response.redirect(`https://github.com/login/oauth/authorize?client_id=${clinet_id_oauth}`);
-})
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_GIT_HUB_CLIENT_ID}`
+    );
+});
 
-//callback
 app.get("/signin/callback", (request, response) => {
     const { code } = request.query;
-    return response.json(code)
-})
 
-app.listen(PORT, () => console.log(`🚀 app is running PORT=${PORT} ... `))
+    return response.json(code);
+});
+
+export { serverHttp, io };
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AutheticateUserController } from "./controllers/AutheticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAutheticated } from "./middleware/ensureAutheticate";
 
 const router = Router();
@@ -8,6 +9,7 @@ const router = Router();
 router.post("/autheticate", new AutheticateUserController().handle);
 
 
-router.post("/messages", ensureAutheticated, new CreateMessageController().handle)
+router.post("/messages", ensureAutheticated, new CreateMessageController().handle);
+router.get("/messages/last3", new Get3LastMessagesController().handle);
 
 export { router }
